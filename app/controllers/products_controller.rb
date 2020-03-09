29a65d479491826fa9f6609785453766f14815b6
Sneha Sabu <snesheb@gmail.com>
2020-03-09 13:33:09 +0000

    Ordering the products by id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Products.search(params[:term]).paginate(page: params[:page], per_page: 10)
+    @products = Products.search(params[:term]).paginate(page: params[:page], per_page: 10).order('id ASC')
    
   end
 
