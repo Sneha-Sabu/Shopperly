27228dbacbcc33fcbdf661ab7f5fc9d6abc2b98b
Sneha Sabu <snesheb@gmail.com>
2020-03-16 17:17:01 +0000

    Emailing

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'sneha.sabu2020@gmail.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Shopperly_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'shopperly.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+  user_name:      Rails.application.secrets.mail_username,
+  password:       Rails.application.secrets.mail_password,
+  domain:         'gmail.com',
+  address:       'smtp.gmail.com',
+  port:          '587',
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
