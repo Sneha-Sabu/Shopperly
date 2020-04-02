71bb840adf4b5c126dcc3b9c8460b1fbb03b1133
Sneha Sabu <snesheb@gmail.com>
2020-04-02 14:38:15 +0000

    Updating the javascript tag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-global.toastr = require("toastr")
+global.toastr = require("toastr");
 
 import "../stylesheets/application"
 import './bootstrap_custom.js'
