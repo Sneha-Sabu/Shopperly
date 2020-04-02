1228789533929539a68d344cdb8e8b63e2b00ca7
Sneha Sabu <snesheb@gmail.com>
2020-04-02 20:35:33 +0000

    Test for Categories model

--- test/models/categories_test.rb
+++ test/models/categories_test.rb
@@ -4,4 +4,10 @@ class CategoriesTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+  product = Categories.new
+  assert product.invalid?
+  assert product.errors[:name].any?
+  
+  end
 end
