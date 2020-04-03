0a6d9a21d291a5a0f2aa01f5efc8a8f2aacb5652
Sneha Sabu <snesheb@gmail.com>
2020-04-03 00:12:11 +0000

    Homepage Dynamic products listing

--- app/controllers/shopperly_controller.rb
+++ app/controllers/shopperly_controller.rb
@@ -1,5 +1,6 @@
 class ShopperlyController < ApplicationController
   skip_before_action :authenticate_users!, :only => [:index]
   def index
+    @products = Products.paginate(page: params[:page], per_page: 10).limit('5').order('id ASC')
   end
 end
