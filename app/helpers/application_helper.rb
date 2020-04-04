07c38289028f5d454eef5e9e286929c28937f9e0
Sneha Sabu <snesheb@gmail.com>
2020-04-04 21:26:25 +0000

    Cart - Remove item functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-    def cart_count_over_one
+
+  def cart_count_over_one
     if @cart.line_item.count > 0
       return "<span class='tag is-dark'>#{@cart.line_item.count}</span>".html_safe
     end
-    end
+  end
 
   def cart_has_item
     return @cart.line_item.count > 0
