46f10e6f5083b4214c224dc6415fc56dcd5b7bb8
Sneha Sabu <snesheb@gmail.com>
2020-04-06 16:10:04 +0000

    Serve static assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,4 +3,12 @@
 //= link jquery.min.js
 //= link bootstrap.bundle.min.js
 //= link jquery.easing.min.js
+//= link validate.js
+//= link jquery.sticky.js
+//= link owl.carousel.min.js
+//= link jquery.waypoints.min.js
+//= link counterup.min.js
+//= link isotope.pkgd.min.js
+//= link venobox.min.js
+//= link main.js
 import '../stylesheets/application'
