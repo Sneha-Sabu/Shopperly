1c48a908c5f35abe55c5d478b3d3da8856165a5b
Ubuntu <ubuntu@ip-172-31-31-227.eu-west-2.compute.internal>
2020-04-06 21:13:53 +0000

    test

--- app/models/products.rb
+++ app/models/products.rb
@@ -2,10 +2,7 @@ class Products < ApplicationRecord
 before_destroy :not_referenced_by_any_line_item
 validates :name, :description, :image, :price, :brand, :categories_id, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
-validates :image, allow_blank: true, format: {
-with: %r{\.(gif|jpg|png)\Z}i,
-message: 'must be a URL for GIF, JPG or PNG image.'
-}
+
   
   include RailsAdminCharts
   belongs_to :categories
