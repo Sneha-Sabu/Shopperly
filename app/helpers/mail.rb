cc4db3ead9b3f568c03b1e7076ccadb275625e06
Sneha Sabu <snesheb@gmail.com>
2020-04-09 23:00:42 +0000

    Emailing through Sendgrid

--- app/helpers/mail.rb
+++ app/helpers/mail.rb
@@ -1,13 +1,13 @@
 require 'sendgrid-ruby'
 include SendGrid
 
-from = Email.new(email: 'test@example.com')
+from = Email.new(email: 'sneha.sabu2020@gmail.com')
 subject = 'Hello World from the SendGrid Ruby Library!'
-to = Email.new(email: 'test@example.com')
+to = Email.new(email: 't17ss19@abdn.ac.uk')
 content = Content.new(type: 'text/plain', value: 'Hello, Email!')
 mail = Mail.new(from, subject, to, content)
 
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+sg = SendGrid::API.new(api_key: ENV['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'])
 response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
