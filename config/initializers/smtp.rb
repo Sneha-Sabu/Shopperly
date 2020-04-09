a847c6b3b520faa7d07a4e901458b07cd58c99e8
Sneha Sabu <snesheb@gmail.com>
2020-04-09 23:27:12 +0000

    Emailing through Sendgrid

--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  domain: 'shopperly.herokuapp.com',
-  user_name: 'app164824422@heroku.com',
-  password: 'aktgs4hx4370',
-  authentication: :login,
-  enable_starttls_auto: true
-}
