ec366995aba6fbf3d4add68188429bf7c6f3fc13
Sneha Sabu <snesheb@gmail.com>
2020-04-10 02:17:09 +0000

    Final layout edits

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -61,7 +61,7 @@ class CartController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Cart was successfully emptied.' }
       format.json { head :no_content }
     end
   end
