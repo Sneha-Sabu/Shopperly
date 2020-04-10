2bc50db1e41f20db94714b257e4ee5896e3b0be1
Sneha Sabu <snesheb@gmail.com>
2020-04-10 15:43:08 +0000

    Recaptcha issue

--- app/models/users.rb
+++ app/models/users.rb
@@ -6,5 +6,7 @@ class Users < ApplicationRecord
   devise :zxcvbnable
 
   # Optionally add more weak words to check against:
-      
+  def weak_words
+    [self.name, self.email]
+  end       
 end
